812a7ab91b467c5404f8bb83e7ddc9364c23f646
boyninja15 <flappyfloorg@gmail.com>
2025-05-26 12:54:02 -0500

    Fixed termology in loading modal

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -2,5 +2,5 @@
 	class="bg-background-2 mx-auto my-[10rem] flex max-w-[585px] flex-col rounded-[.5rem] p-[1rem] shadow-md"
 >
 	<span class="text-center text-[2rem] font-semibold">FloorTech Docs</span>
-	<span class="text-foreground-2 text-center">We are checking if the app is down...</span>
+	<span class="text-foreground-2 text-center">We are checking if the app is online...</span>
 </div>
